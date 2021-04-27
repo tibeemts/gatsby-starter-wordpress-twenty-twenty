4a51a45bbf31f718c3e840f4491872aa32c5d266
Peter Hordijk <rogha@peter-hordijk.com>
2021-04-27 23:05:16 +0000

    Initial commit

---
+++ src/fragments.js
@@ -0,0 +1,105 @@
+import { graphql } from "gatsby"
+
+export const fragments = graphql`
+  fragment HeroImage on File {
+    childImageSharp {
+      gatsbyImageData(layout: CONSTRAINED, quality: 90)
+    }
+  }
+
+  fragment AvatarImage on File {
+    childImageSharp {
+      gatsbyImageData(layout: FIXED, width: 80, height: 80, quality: 90)
+    }
+  }
+
+  fragment Thumbnail on File {
+    childImageSharp {
+      gatsbyImageData(layout: CONSTRAINED, quality: 90)
+    }
+  }
+
+  fragment PostPreviewContent on WpPost {
+    uri
+    title
+    databaseId
+    excerpt
+    date(formatString: "LL")
+    featuredImage {
+      node {
+        altText
+        localFile {
+          ...Thumbnail
+        }
+      }
+    }
+    author {
+      node {
+        name
+        firstName
+        lastName
+        uri
+      }
+    }
+    categories {
+      nodes {
+        name
+        slug
+        uri
+      }
+    }
+  }
+
+  fragment PostContent on WpPost {
+    title
+    content
+    date(formatString: "LL")
+    excerpt
+    featuredImage {
+      node {
+        altText
+        localFile {
+          ...HeroImage
+        }
+      }
+    }
+    author {
+      node {
+        name
+        firstName
+        lastName
+        uri
+        description
+        avatar {
+          url
+          width
+          height
+          imageFile {
+            ...AvatarImage
+          }
+        }
+      }
+    }
+    categories {
+      nodes {
+        name
+        slug
+        uri
+      }
+    }
+  }
+
+  fragment PageContent on WpPage {
+    title
+    content
+    databaseId
+    featuredImage {
+      node {
+        altText
+        localFile {
+          ...HeroImage
+        }
+      }
+    }
+  }
+`
